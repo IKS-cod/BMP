727ea605d73cbbf436f7fdd7109e22f73444b52b
IKS-cod <ik.smirnov@yandex.ru>
2024-10-15 20:16:54 +0300

    выполнил задание BMP-29
    Исправил id в тестах class RecommendationsServiceTest, добавил @MockBean
     private RulesRecommendationsRepository rulesRecommendationsRepository;

--- src/test/java/BMP/service/RecommendationsServiceTest.java
+++ src/test/java/BMP/service/RecommendationsServiceTest.java
@@ -6,6 +6,7 @@ import BMP.interfaces.RecommendationRuleSetTopSaving;
 import BMP.model.ModelJSon;
 import BMP.model.Recommendation;
 import BMP.repository.RecommendationsRepository;
+import BMP.repository.RulesRecommendationsRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -26,6 +28,9 @@ class RecommendationsServiceTest {
     private MockMvc mockMvc;
     @MockBean
     private RecommendationsRepository repository;
+
+    @MockBean
+    private RulesRecommendationsRepository rulesRecommendationsRepository;
 
     @SpyBean
     private RecommendationsService recommendationsService;
@@ -40,9 +45,12 @@ class RecommendationsServiceTest {
     void checkGetModelJSonPositiveTest() {
         String id = "1f9b149c-6577-448a-bc94-16bea229b71a";
         List<Recommendation> recommendationList = new ArrayList<>();//expected
-        Recommendation recommendation11 = new Recommendation("Invest 500", 1, "Описание Инвест 500");
-        Recommendation recommendation22 = new Recommendation("Simple Credit", 3, "Описание Simple credit");
-        Recommendation recommendation33 = new Recommendation("Top Saving", 2, "Описание Top Saving");
+        Recommendation recommendation11 = new Recommendation("Invest 500",
+                UUID.fromString("147f6a0f-3b91-413b-ab99-87f081d60d5a"), "Описание Инвест 500");
+        Recommendation recommendation22 = new Recommendation("Simple Credit",
+                UUID.fromString("ab138afb-f3ba-4a93-b74f-0fcee86d447f"), "Описание Simple credit");
+        Recommendation recommendation33 = new Recommendation("Top Saving",
+                UUID.fromString("59efc529-2fff-41af-baff-90ccd7402925"), "Описание Top Saving");
         recommendationList.add(recommendation11);
         recommendationList.add(recommendation22);
         recommendationList.add(recommendation33);
@@ -59,10 +67,14 @@ class RecommendationsServiceTest {
     void checkGetModelJSonNegativeTest() {
         String id = "1f9b149c-6577-448a-bc94-16bea229b71a";
         List<Recommendation> recommendationList = new ArrayList<>();//expected
-        Recommendation recommendation11 = new Recommendation("Invest 500", 1, "Описание Инвест 500");
-        Recommendation recommendation22 = new Recommendation("Simple Credit", 3, "Описание Simple credit");
-        Recommendation recommendation33 = new Recommendation("Top Saving", 2, "Описание Top Saving");
-        Recommendation recommendation44 = new Recommendation("Test", 5, "Описание Test");
+        Recommendation recommendation11 = new Recommendation("Invest 500",
+                UUID.fromString("147f6a0f-3b91-413b-ab99-87f081d60d5a"), "Описание Инвест 500");
+        Recommendation recommendation22 = new Recommendation("Simple Credit",
+                UUID.fromString("ab138afb-f3ba-4a93-b74f-0fcee86d447f"), "Описание Simple credit");
+        Recommendation recommendation33 = new Recommendation("Top Saving",
+                UUID.fromString("59efc529-2fff-41af-baff-90ccd7402925"), "Описание Top Saving");
+        Recommendation recommendation44 = new Recommendation("Test", UUID.fromString
+                ("59efc529-2fff-41af-baff-90ccd7402930"), "Описание Test");
         recommendationList.add(recommendation11);
         recommendationList.add(recommendation22);
         recommendationList.add(recommendation33);
