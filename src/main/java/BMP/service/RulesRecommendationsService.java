1e5483e6497265796d73c33877863d7de6bf388c
user <Seregawinner92@gmail.com>
2024-10-22 19:49:24 +0300

    выполнил задание BMP-42
    1.Создал GET-запросы "/rule/stats" и "/userId" в RecommendationsController;
    2.Перераписал методы getStats и getUserId в RecommendationsService;
    3.Добавил счетчик рекомендаций в методт get в RecommendationsService;
    4.Переписал методы createRulesRecommendations и deleteRulesRecommendations в RulesRecommendationsService.

--- src/main/java/BMP/service/RulesRecommendationsService.java
+++ src/main/java/BMP/service/RulesRecommendationsService.java
@@ -3,8 +3,10 @@ package BMP.service;
 
 import BMP.model.Product;
 import BMP.model.QueryRecommendation;
+import BMP.model.Stats;
 import BMP.repository.QueryRecommendationRepository;
 import BMP.repository.RulesRecommendationsRepository;
+import BMP.repository.StatsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,7 @@ import java.util.Objects;
  */
 @Service
 public class RulesRecommendationsService {
+    private final StatsRepository statsRepository;
     private static final Logger logger = LoggerFactory.getLogger(RulesRecommendationsService.class);
     private final RulesRecommendationsRepository rulesRecommendationsRepository;
     private final QueryRecommendationRepository queryRecommendationRepository;
@@ -28,42 +31,36 @@ public class RulesRecommendationsService {
      * @param rulesRecommendationsRepository  Репозиторий правил рекомендаций.
      * @param queryRecommendationRepository    Репозиторий запросов рекомендаций.
      */
-    public RulesRecommendationsService(RulesRecommendationsRepository rulesRecommendationsRepository,
+    public RulesRecommendationsService(StatsRepository statsRepository,
+                                       RulesRecommendationsRepository rulesRecommendationsRepository,
                                        QueryRecommendationRepository queryRecommendationRepository) {
+        this.statsRepository = statsRepository;
         this.rulesRecommendationsRepository = rulesRecommendationsRepository;
         this.queryRecommendationRepository = queryRecommendationRepository;
     }
 
-    /**
-     * Создает правила рекомендаций для указанного продукта.
-     *
-     * @param product Продукт, для которого создаются правила рекомендаций.
-     * @return Сохраненный продукт с правилами рекомендаций.
-     */
     public Product createRulesRecommendations(Product product) {
-        if ( Objects.isNull(product)) {
+        if (Objects.isNull(product)) {
             throw new IllegalArgumentException("Некорректные условия");
         }
         if (product.getRule().isEmpty() || Objects.isNull(product.getRule())) {
             throw new IllegalArgumentException("Некорректные условия");
         }
+
         List<QueryRecommendation> queryRecommendationList = product.getRule();
         logger.info("Создание правил рекомендаций для продукта: {}", product.getProductName());
 
         // Сохраняем каждое правило рекомендации
         for (QueryRecommendation queryRecommendation : queryRecommendationList) {
             queryRecommendationRepository.save(queryRecommendation);
-            logger.info("Сохранено правило рекомендации: {}", queryRecommendation);
+            logger.debug("Сохранено правило рекомендации: {}", queryRecommendation);
         }
+
+        Stats stats = new Stats(product.getProductId(), 0);
+        statsRepository.save(stats);
 
         return rulesRecommendationsRepository.save(product);
     }
-
-    /**
-     * Получает все запросы рекомендаций.
-     *
-     * @return Коллекция всех запросов рекомендаций.
-     */
     public Collection<QueryRecommendation> getAllQueryRecommendations() {
         logger.info("Вызван метод для получения всех запросов рекомендаций.");
         Collection<QueryRecommendation> productCollection = queryRecommendationRepository.findAll();
@@ -91,14 +88,13 @@ public class RulesRecommendationsService {
      */
     public void deleteRulesRecommendations(long id) {
         logger.info("Вызван метод для удаления правила рекомендации с id: {}", id);
-
-        // Проверяем наличие правила перед удалением
         if (!rulesRecommendationsRepository.existsById(id)) {
-            logger.info("Правило рекомендации с id = {} не найдено", id);
+            logger.error("Правило рекомендации с id = {} не найдено", id);
             throw new RuntimeException("Рекомендация с id= %d не найдена".formatted(id));
         }
-
+        statsRepository.deleteById(rulesRecommendationsRepository.findById(id).get().getProductId());
         rulesRecommendationsRepository.deleteById(id);
+
         logger.info("Удалено правило рекомендации с id: {}", id);
     }
 }
